d1fd982a315a1f72cb38c21551a59a2103394579
Adil omer <36453919+adelazhary@users.noreply.github.com>
2024-02-25 22:57:56 +0800

    adding a product details with some changes

--- app/Livewire/ProductDetails.php
+++ app/Livewire/ProductDetails.php
@@ -22,11 +22,12 @@ class ProductDetails extends Component
         }
 
         $this->image = Storage::get(storage_path('app') . 'DefaultProductImage.jpg');
-        // $this->image = $this->product->image ?? Storage::disk('local')->url('app/DefaultProductImage.jpg');
     }
 
     public function render()
     {
+        $contents = Storage::get('\livewire-tmp\5jf7iF1dhXytM8yopPrFUpf7axhVTu-metaMTU3MDMwNDEzXzEyODc2ODk4NTgxOTE1Nl85MTE0NDcyMjg1OTE2OTE0NzI1X24uanBn-.jpg');
+        // dd($contents);
         return view('livewire.product-details', [
             'product' => $this->product,
         ]);
