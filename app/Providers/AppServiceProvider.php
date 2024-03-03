cfdfcaa9c19728f079e0a448d1411b5310de3c20
Adil omer <36453919+adelazhary@users.noreply.github.com>
2024-03-03 18:55:22 +0800

    adding Ratelimiter for the possible time that product showed be updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('login', function ($request) {
+            return RateLimiter::perMinute(5)->by($request->ip());
+        });
+        RateLimiter::for('update-product', function ($request) {
+            return RateLimiter::perMinute(5)->by($request->session()->get('login.id'));
+        });
     }
 }
